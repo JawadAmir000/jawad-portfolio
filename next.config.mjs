9885acef81117d7d33f1377978bbdd9c720b4ba0
JawadAmir000 <jawadamirselise@gmail.com>
2025-12-17 00:55:34 +0600

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    unoptimized: false,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/jawad-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/jawad-portfolio/' : '',
 };
 
 export default nextConfig;
